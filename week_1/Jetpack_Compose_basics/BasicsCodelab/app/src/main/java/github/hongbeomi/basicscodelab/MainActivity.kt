b7da8e2ee31ecc3fc21e8ce3a028c3552fcacb3d
hongbeomi <dks3963@naver.com>
2021-11-02 20:06:30 +0900

    App Style, Theme

--- week_1/Jetpack_Compose_basics/BasicsCodelab/app/src/main/java/github/hongbeomi/basicscodelab/MainActivity.kt
+++ week_1/Jetpack_Compose_basics/BasicsCodelab/app/src/main/java/github/hongbeomi/basicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package github.hongbeomi.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import github.hongbeomi.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -41,7 +43,6 @@ private fun MyApp() {
 
 @Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -60,7 +61,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
             Greeting(name = name)
@@ -98,7 +99,12 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
@@ -109,7 +115,15 @@ private fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    showBackground = true, widthDp = 320
+)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
