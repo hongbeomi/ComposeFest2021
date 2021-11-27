6888be48b7b65ea0fdf7fdd70879bd1095eb4935
hongbeomi <dks3963@naver.com>
2021-11-27 13:20:04 +0900

    compose 4주차 - Merged and unmerged Semantics trees

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TobAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TobAppBarTest.kt
@@ -50,11 +50,18 @@ class TobAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
-
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(
+                    RallyScreen.Accounts.name.uppercase()
+                ) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 
